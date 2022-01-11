6bea477889e631340813af606a580b65234af5fc
cczr_glb <cczr_glb@qq.com>
2022-01-11 17:40:47 +0800

    更新rpc配置的枚举类

---
+++ rpc-framework-common/src/main/java/github/rpc/enums/RpcConfigEnum.java
@@ -0,0 +1,16 @@
+package github.rpc.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+// rpc配置相关的枚举
+
+@AllArgsConstructor
+@Getter
+public enum RpcConfigEnum {
+
+    RPC_CONFIG_PATH("rpc.properties"),
+    ZK_ADDRESS("rpc.zookeeper.address");
+
+    private final String propertyValue;
+}
